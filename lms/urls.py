e3638040553ea4ff2c1a8286d225a0d784398169
mixei1902 <misei190289@gmail.com>
2024-07-12 16:32:09 +0300

    add gitignore env

--- lms/urls.py
+++ lms/urls.py
@@ -11,11 +11,11 @@ router = SimpleRouter()
 router.register('', CourseViewSet)
 
 urlpatterns = [
-    path("lms/", LessonListAPIView.as_view(), name="lessons_list"),
-    path("lms/<int:pk>", LessonRetrieveAPIView.as_view(), name="lessons_retrieve"),
-    path("lms/create/", LessonCreateAPIView.as_view(), name="lessons_create"),
-    path("lms/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lessons_delete"),
-    path("lms/<int:pl>,/update/", LessonUpdateAPIView.as_view(), name="lessons_update"),
+    path("les/", LessonListAPIView.as_view(), name="lessons_list"),
+    path("les/<int:pk>", LessonRetrieveAPIView.as_view(), name="lessons_retrieve"),
+    path("les/create/", LessonCreateAPIView.as_view(), name="lessons_create"),
+    path("les/<int:pk>/delete/", LessonDestroyAPIView.as_view(), name="lessons_delete"),
+    path("les/<int:pl>,/update/", LessonUpdateAPIView.as_view(), name="lessons_update"),
 
 ]
 urlpatterns += router.urls
